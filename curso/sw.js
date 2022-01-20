e1fa390db08dc27fa9c7962fc27795c820ddb024
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-01-20 19:48:35 -0300

    up date

--- curso/sw.js
+++ curso/sw.js
@@ -1,5 +1,5 @@
 const cacheName = 'v2';
-const resourcesToPrecache = ['/','./index.html','./manifest.html','./192x192.png','./512x512.png','./js/one.js'];
+const resourcesToPrecache = ['/','./index.html','./manifest.json','./192x192.png','./512x512.png','./index.js'];
 
 /*self.addEventListener('install', (event) => {
   event.waitUntil(
